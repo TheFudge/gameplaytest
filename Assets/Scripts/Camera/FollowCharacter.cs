7ee0930d3b8e6a21dbc83196af6753328f5253ba
Kasimir Blust <mail@kasimir-blust.de>
2019-12-01 10:35:36 +0100

    ADD: Follow camera

---
+++ Assets/Scripts/Camera/FollowCharacter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetToFollow;
+    private bool followingTarget = false;
+
+    [SerializeField]
+    private bool lerpToObject = true;
+    [Range(0f, 20f)]
+    [SerializeField]
+    private float lerpSpeed = 4f;
+    
+    [SerializeField]
+    private Vector3 cameraOffset = Vector3.zero;
+
+    private float cameraZPosition = -10f;
+    
+    void Start()
+    {
+        followingTarget = targetToFollow != null;
+    }
+
+    public void Follow(Transform target)
+    {
+        targetToFollow = target;
+        followingTarget = true;
+    }
+
+    public void StopFollowing(Transform target)
+    {
+        if (targetToFollow == target)
+            return;
+        targetToFollow = null;
+        followingTarget = false;
+    }
+
+    void LateUpdate()
+    {
+        if (!followingTarget)
+            return;
+        
+        if (lerpToObject)
+        {
+            DoLerpedFollow();
+        }
+        else
+        {
+            DoHardFollow();
+        }
+    }
+
+    void DoLerpedFollow()
+    {
+        Vector3 pos = Vector3.Lerp(transform.position, targetToFollow.position + cameraOffset, Time.deltaTime * lerpSpeed);
+        pos.z = cameraZPosition;
+        transform.position = pos;
+
+    }
+
+    void DoHardFollow()
+    {
+        transform.position = targetToFollow.position + cameraOffset;
+    }
+}
